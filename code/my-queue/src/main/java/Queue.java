3e9e9aa0e187fe9cfaedadc42d96d63b9843d3b6
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2017-09-05 19:25:52 -0300

    first commit

---
+++ code/my-queue/src/main/java/Queue.java
@@ -0,0 +1,73 @@
+import java.util.Arrays;
+
+public class Queue<E> {
+
+	private E[] queueList;
+    private int maxSize;
+	private int head;
+    private int tail;
+
+    Queue(int size) {
+        this.maxSize = size;
+        this.queueList = (E[]) new Object[size];
+        this.head = -1;
+        this.tail = -1;
+    }
+
+    int getSize(){
+    	if(this.isEmpty()){
+    		return 0;
+    	} else {
+    		return (this.tail - this.head) + 1;
+    	}
+    }
+
+    E getFirst(){
+    	return this.queueList[this.head];
+    }
+
+    void insert(E value) throws FullQueueException {
+    	// If queue is full
+        if ((tail + 1) % maxSize == head) {
+            throw new FullQueueException("Queue is full");
+        // If queue is empty
+        } else if (this.isEmpty()) {
+            head++;
+            tail++;
+            queueList[tail] = value;
+        // If queue has > 1 element
+        } else {
+            tail = (tail + 1) % maxSize;
+            queueList[tail] = value;
+        }
+    }
+
+    void remove() throws EmptyQueueException {
+        // If queue is empty
+        if (this.isEmpty()) {
+            throw new EmptyQueueException("Queue is empty");
+        // If queue has 1 element
+        } else if (head == tail) {
+            // Empty list indexes
+            head = -1;
+            tail = -1;
+         // If queue has > 1 element
+        } else {
+            // Relocate head
+            head = (head + 1) % maxSize;
+        }
+    }
+
+    boolean isEmpty() {
+        return (head == -1 && tail == -1);
+    }
+
+    @Override
+    public String toString() {
+        return "(h:" + head
+        		+ ", t:" + tail
+        		+ ", s:" + maxSize
+                + ") queue = " + Arrays.toString(queueList);
+    }
+
+}
---
+++ code/my-queue/src/main/java/main.java
@@ -0,0 +1,31 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 * @throws EmptyQueueException
+	 */
+	public static void main(String[] args) {
+		Queue<String> newStringQueue = new Queue<String>(3);
+
+		try {
+			newStringQueue.insert("banana");
+			newStringQueue.insert("orange");
+			newStringQueue.insert("apple");
+	        System.out.println(newStringQueue.toString());
+			System.out.println(newStringQueue.getFirst());
+	        System.out.println(newStringQueue.getSize());
+
+	        newStringQueue.remove();
+	        System.out.println(newStringQueue.toString());
+			System.out.println(newStringQueue.getFirst());
+			System.out.println(newStringQueue.getSize());
+
+		} catch (FullQueueException e) {
+			e.printStackTrace();
+		} catch (EmptyQueueException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
