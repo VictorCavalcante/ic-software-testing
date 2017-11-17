3474bdde741a867e0d50223068fee235a710aaf2
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2017-11-17 01:28:20 -0300

    Refactoring MyQueue.java to use the primitive array instead of the ArrayList Class.

--- code/aluno-08/src/main/java/br/ufal/ic/my/queue/MyQueue.java
+++ code/aluno-08/src/main/java/br/ufal/ic/my/queue/MyQueue.java
@@ -1,16 +1,13 @@
-
-
 package br.ufal.ic.my.queue;
-
-import java.util.ArrayList;
 
 /**
  *
  * @author julia
  */
 public class MyQueue<T> {
-    
-    private ArrayList<T> queue;
+
+    private T[] queue;
+    private int maxSize;
     private int first;
     private int last;
     private int currentSize;
@@ -20,23 +17,42 @@ public class MyQueue<T> {
     @param Integer which corresponds to the size of the queue.
      */
     public MyQueue(int size) {
-        this.queue = new ArrayList<T>();
+        this.maxSize = size;
+        this.queue = (T[]) new Object[size];
         this.first = 0;
         this.last = size - 1;
         this.currentSize = 0;
+    }
+
+    public int getSize(){
+        if(this.isEmpty()){
+            return 0;
+        } else {
+            return (this.last - this.first) + 1;
+        }
+    }
+
+    public boolean isEmpty() {
+        return (first == -1 && last == -1);
     }
     
     /*
     Inserts an element at the end of the queue.
      */
-    public void enqueue(T element) {
-        if (currentSize == queue.size()){
+    public void add(T element) {
+        // If queue is full
+        if ((last + 1) % maxSize == first) {
             throw new QueueIsFullException("Queue está com tamanho máximo.");
+            // If queue is empty
+        } else if (this.isEmpty()) {
+            first++;
+            last++;
+            queue[last] = element;
+            // If queue has > 1 element
         } else {
-            queue.add(element);
-            currentSize++;
-        }
-            
+            last = (last + 1) % maxSize;
+            queue[last] = element;
+        }            
     }
     
      /*
@@ -53,7 +69,7 @@ public class MyQueue<T> {
         if (currentSize == 0) {
             throw new QueueIsFullException("Queue está vazia.");
         } else {
-            T element = queue.get(first);
+            T element = queue[first];
             
             return element;
         }
@@ -63,14 +79,20 @@ public class MyQueue<T> {
     Removes the queue first element and returns it.
     */
     public T dequeue() {
-        if (currentSize == 0) {
+        T returnedValue;
+        // If queue is empty
+        if (this.isEmpty()) {
             throw new QueueIsFullException("Queue está vazia.");
         } else {
-            T element = queue.get(first);
-            queue.add(first,null);
-            this.currentSize--;
-            
-            return element;
+            returnedValue =  queue[first];
+            // If queue has 1 element
+            if (first == last) {
+                first = -1; last = -1; // Empty list indexes
+            // If queue has > 1 element
+            } else {
+                first = (first + 1) % maxSize; // Relocate first
+            }
+            return returnedValue;
         }
     }
     
@@ -81,11 +103,11 @@ public class MyQueue<T> {
         
         String string = "QueuePack = {";
         
-        for (int i = 0; i < queue.size(); i++) {
-            if (i < queue.size() - 1) {
-                string += queue.get(i) + ", ";
+        for (int i = 0; i < this.getSize(); i++) {
+            if (i < this.getSize() - 1) {
+                string += queue[i] + ", ";
             } else {
-                string += queue.get(i);
+                string += queue[i];
             }
         }
         
@@ -97,7 +119,7 @@ public class MyQueue<T> {
     //Reboot the queue
     public void resetQueue() {
         
-        for (int i = 0; i < queue.size(); i++) {
+        for (int i = 0; i < this.getSize(); i++) {
             this.dequeue();
         }
         
